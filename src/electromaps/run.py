cc3d77161aca39bed9d29fb9d86ff25504b3f084
unknown <ashirov.daniil2108@gmail.com>
2023-05-04 23:10:53 +0400

    fix linter

--- src/electromaps/run.py
+++ src/electromaps/run.py
@@ -4,12 +4,12 @@ from parsers.settings import ElectromapsSettings
 settings = ElectromapsSettings()
 
 
-def request(data: dict):
+def request(data: dict) -> dict:
     req = requests.post(settings.URL_EM, json=data)
     return req.json()
 
 
-def processing_data(locations_dict) -> list[dict[str, int | str | float]]:
+def processing_data(locations_dict: dict) -> list[dict[str, int | str | float]]:
     result = []
     for location in locations_dict:
         result.append({
@@ -21,7 +21,7 @@ def processing_data(locations_dict) -> list[dict[str, int | str | float]]:
     return result
 
 
-def electromaps_parser():
+def electromaps_parser() -> list[dict[str, int | str | float]]:
     data = settings.LONDON_COORDINATES
     return processing_data(request(data))
 
