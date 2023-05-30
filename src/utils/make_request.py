1250d83b7b6a10f2ca4a93e64da980cfb1b7491e
Mark Antipin <markantipin@Marks-MacBook-Pro.local>
2023-05-30 11:35:17 +0200

    add some updates

---
+++ src/utils/make_request.py
@@ -0,0 +1,44 @@
+import time
+from enum import StrEnum, auto
+from logging import getLogger
+
+import requests
+
+logger = getLogger(__name__)
+
+
+class RequestMethod(StrEnum):
+    GET = auto()
+    POST = auto()
+
+
+def make_request(
+        url: str,
+        data: dict = None,
+        method: str = RequestMethod.GET,
+        timeout: float = 10,
+        retries: int = 2,
+        sleep_time: float = 1,
+) -> requests.Response | None:
+    try:
+        r = requests.request(
+            method=method,
+            url=url,
+            data=data,
+            timeout=timeout,
+        )
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+        if retries > 0:
+            time.sleep(sleep_time)
+            return make_request(
+                url=url,
+                data=data,
+                method=method,
+                timeout=timeout,
+                retries=retries - 1,
+            )
+        logger.info(f'Failed to make request to {method.upper()} {url}')
+        return None
+    return r
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,9 @@ from settings import BASE_DIR
 
 PLACES_JSON = Path(BASE_DIR, 'tests', 'data', 'chargemap_places.json')
 
+
 @pytest.fixture()
 def chargemap_places_json() -> dict:
-    with open(PLACES_JSON) as file:
+    with Path.open(PLACES_JSON) as file:
         result = json.load(file)
     return result
