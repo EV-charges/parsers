2699dc6c903b95728d2e4ce31709762b4fafd44a
unknown <ashirov.daniil2108@gmail.com>
2023-06-28 00:11:55 +0400

    fix tests

--- src/electromaps/run.py
+++ src/electromaps/run.py
@@ -7,7 +7,7 @@ import schedule
 from settings import AllParsersSettings, ApiSettings, ElectromapsSettings
 from src.utils.getting_id_places_from_db import getting_id_places_from_db
 from src.utils.make_request import RequestMethod, make_request
-from src.utils.make_request_proxy import make_request_proxy
+#from src.utils.make_request_proxy import make_request_proxy
 
 settings = ElectromapsSettings()
 api_settings = ApiSettings()
@@ -55,7 +55,7 @@ def processing_comments(
 def places_parsing() -> list[dict] | None:
     coordinates = settings.coordinates
     response = make_request(url=settings.PLACES_URL, params=coordinates)
-    response = make_request_proxy(url=settings.PLACES_URL + coordinates, retries_proxy=15, method=RequestMethod.GET)
+    #response = make_request_proxy(url=settings.PLACES_URL + coordinates, retries_proxy=15, method=RequestMethod.GET)
 
     if not response:
         return
