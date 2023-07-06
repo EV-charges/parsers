ab88c5ba8380e36753dbf30ea8d69caa0ca1e458
unknown <ashirov.daniil2108@gmail.com>
2023-07-06 17:55:13 +0400

    fix tests

--- tests/chargemap/test_processing_places_and_save_db.py
+++ tests/chargemap/test_processing_places_and_save_db.py
@@ -2,27 +2,27 @@ from unittest.mock import Mock
 
 import pytest
 
-from src.chargemap.run import data_processing_and_save_db
+from src.chargemap.run import processing_places_and_save_db
 
 
 def test_data_processing_count_zero(set_id: set[int]) -> None:
-    result = data_processing_and_save_db({'count': 0}, set_id)
+    result = processing_places_and_save_db({'count': 0}, set_id)
     assert result == 0
 
 
 def test_data_processing_logging_cluster(set_id: set[int]) -> None:
     response = {"count": 1, "items": [{"type": "cluster", "lat": 1, "lng": 2}]}
-    result = data_processing_and_save_db(response, set_id)
+    result = processing_places_and_save_db(response, set_id)
     assert result == 0
 
 
 def test_data_processing_error_wrong_key(set_id: set[int]) -> None:
     response = {"count": 1, "items": [{"type": "pool"}]}
     with pytest.raises(KeyError):
-        data_processing_and_save_db(response, set_id)
+        processing_places_and_save_db(response, set_id)
 
 
 def test_data_processing_correct_return(mock_make_request: Mock, chargemap_places_json: dict, set_id: set[int]) -> None:
     mock_make_request.return_value = True
-    result = data_processing_and_save_db(chargemap_places_json, set_id)
+    result = processing_places_and_save_db(chargemap_places_json, set_id)
     assert result == 1
